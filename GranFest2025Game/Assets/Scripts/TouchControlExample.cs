9e9dd535c2261daaee40fd33279ffbf467823ad1
kg718 <kggoodall18@gmail.com>
2025-05-27 15:28:15 +0100

    Commit Turns

---
+++ GranFest2025Game/Assets/Scripts/TouchControlExample.cs
@@ -0,0 +1,64 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+
+[RequireComponent(typeof(TouchSimulation))]
+public class TouchControlExample : MonoBehaviour
+{
+    [HideInInspector] public Transform[] points;
+
+    [SerializeField] private GameObject trackPointPrefab;
+    [SerializeField] private float minDist;
+    [SerializeField] private float pointStep;
+    private float currentPointStep;
+    private Transform lastPoint;
+
+    void Awake()
+    {
+        EnhancedTouchSupport.Enable();   
+    }
+
+    private void Start()
+    {
+        currentPointStep = pointStep;
+    }
+
+    void Update()
+    {
+        currentPointStep -= Time.deltaTime;
+
+        if (Touch.activeFingers.Count == 1 && currentPointStep <= 0)
+        {
+            if(TurnManagement.instance.CheckCanDraw())
+            {
+                foreach (var touchPoint in Touch.activeTouches)
+                {
+                    if (lastPoint == null)
+                    {
+                        DrawPoint(touchPoint.screenPosition);
+                    }
+                    else if (Vector3.Distance(Camera.main.ScreenToWorldPoint(new Vector3(touchPoint.screenPosition.x, touchPoint.screenPosition.y, 10.0f)), lastPoint.transform.position) > minDist)
+                    {
+                        DrawPoint(touchPoint.screenPosition);
+                    }
+                }
+            }
+        }
+        else if(Touch.activeFingers.Count == 0 && !TurnManagement.instance.CheckCanDraw())
+        {
+            TurnManagement.instance.EndTurn();
+        }
+    }
+
+    public void DrawPoint(Vector3 _touchPoint)
+    {
+        GameObject newPoint = Instantiate(trackPointPrefab);
+        Vector3 newPos = Camera.main.ScreenToWorldPoint(_touchPoint);
+        newPoint.transform.position = new Vector3(newPos.x, 0, newPos.z);
+        lastPoint = newPoint.transform;
+        currentPointStep = pointStep;
+        points = points.Append(newPoint.transform).ToArray();
+        TurnManagement.instance.DecrementDrawAmmount();
+    }
+}
---
+++ GranFest2025Game/Assets/Scripts/TurnManagement.cs
@@ -0,0 +1,99 @@
+using UnityEngine;
+
+public class TurnManagement : MonoBehaviour
+{
+    public static TurnManagement instance;
+
+    private int currentTurn = 1;
+    [SerializeField] private float playerDrawAmmount;
+
+    [HideInInspector] public float drawAmount1;
+    [HideInInspector] public float drawAmount2;
+    [HideInInspector] public float drawAmount3;
+    [HideInInspector] public float drawAmount4;
+
+    void Start()
+    {
+        instance = this;
+        drawAmount1 = playerDrawAmmount;
+        drawAmount2 = playerDrawAmmount;
+        drawAmount3 = playerDrawAmmount;
+        drawAmount4 = playerDrawAmmount;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void DecrementDrawAmmount()
+    {
+        switch (currentTurn)
+        {
+            case 1:
+                if (drawAmount1 >= 0)
+                {
+                    drawAmount1--;
+                }
+                break;
+            case 2:
+                if (drawAmount2 >= 0)
+                {
+                    drawAmount2--;
+                }
+                break;
+            case 3:
+                if (drawAmount3 >= 0)
+                {
+                    drawAmount3--;
+                }
+                break;
+            case 4:
+                if (drawAmount4 >= 0)
+                {
+                    drawAmount4--;
+                }
+                break;
+        }
+    }
+
+    public bool CheckCanDraw()
+    {
+        switch (currentTurn)
+        {
+            case 1:
+                if(drawAmount1 <= 0)
+                {
+                    return false;
+                }
+                break;
+            case 2:
+                if (drawAmount2 <= 0)
+                {
+                    return false;
+                }
+                break;
+            case 3:
+                if (drawAmount3 <= 0)
+                {
+                    return false;
+                }
+                break;
+            case 4:
+                if (drawAmount4 <= 0)
+                {
+                    return false;
+                }
+                break;
+        }
+        return true;
+    }
+
+    public void EndTurn()
+    {
+        if(currentTurn <= 3)
+        {
+            currentTurn++;
+        }
+    }
+}
