290b3c87188256fd60516243129a1a6b48ccd004
LotusBlossxm <luke.deacon23@my.northampton.ac.uk>
2025-05-28 09:52:09 +0100

    MainMenu-Transitions
    Level change + Main menu assets

---
+++ GranFest2025Game/Assets/Scripts/LevelTransition.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour
+{
+    private bool started = false;
+
+
+    public void StartLoad()
+    {
+        if (!started)
+        {
+            started = true;
+            StartCoroutine(LoadLevel());
+        }
+    }
+    void OnQuit()
+    {
+        print("Game Quitting!");
+        Application.Quit();
+    }
+
+    IEnumerator LoadLevel()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("Level1");
+    }
+}
