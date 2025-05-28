447c8394d8b1dd8f8c3d31a31650691e47343313
kg718 <kggoodall18@gmail.com>
2025-05-28 14:46:10 +0100

    Commit Hazard Functionality WIP

--- GranFest2025Game/Assets/Scripts/Car.cs
+++ GranFest2025Game/Assets/Scripts/Car.cs
@@ -1,18 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int playerNumber;
+    [SerializeField] private float QTESuccessTime;
+    private float currentQTETime;
+    bool canBeHit = true;
+    QTEManagement.StopHazardDelegate stopHazard;
+
+    private void Start()
     {
-        
+        currentQTETime = QTESuccessTime;
+        stopHazard = OnPressQTE;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(currentQTETime > 0)
+        {
+            currentQTETime -= Time.deltaTime;
+        }
+        else
+        {
+            canBeHit = true;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        print(other.name);
+        if(other.gameObject.tag == "Warning")
+        {
+            QTEManagement.Instance.CreateQTE(stopHazard,playerNumber);
+        }
+        if(other.gameObject.tag == "Hazard" && canBeHit)
+        {
+            
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Warning")
+        {
+            
+        }
+    }
+
+    public void OnPressQTE(int i)
+    {
+        print(i);
+        currentQTETime = QTESuccessTime;
+        canBeHit = false;
     }
 }
